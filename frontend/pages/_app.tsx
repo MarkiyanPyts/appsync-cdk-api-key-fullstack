b3495518ea8de4c5a72ee11ea09302d6ef4c07b1
Markiyan Pyts <markiyan.pyts@osf.digital>
2023-03-03 08:11:51 +0200

    update

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,22 +4,22 @@ import '@aws-amplify/ui-react/styles.css'
 import { Amplify } from 'aws-amplify'
 
 Amplify.configure({
-	aws_project_region: 'us-east-1',
+	aws_project_region: 'eu-central-1',
 	Auth: {
-		region: 'us-east-1',
-		userPoolId: 'us-east-1_KcRwIEqwx',
-		userPoolWebClientId: '2becm8seaiol1gdr3uvr4i8bue',
-		identityPoolId: 'us-east-1:ca5a162a-8e93-4847-9864-1f1fef1c7d9b',
+		region: 'eu-central-1',
+		userPoolId: 'eu-central-1_BIdsMLgSn',
+		userPoolWebClientId: '1n231b393ibrrd1jlcmbb4v6fb',
+		identityPoolId: 'eu-central-1:4e21e0c5-4818-4ea8-b61b-2fd621f7747f',
 	},
 	Storage: {
 		AWSS3: {
-			bucket: 'productfilestoragestack-productbucket674e806c-vu5bd2brtrhy',
-			region: 'us-east-1',
+			bucket: 'productfilestoragestack-productbucket674e806c-1dp5jbkeuj85q',
+			region: 'eu-central-1',
 		},
 	},
 	aws_appsync_graphqlEndpoint:
-		'https://ulgcri74prestbdtt72nm3wpeq.appsync-api.us-east-1.amazonaws.com/graphql',
-	aws_appsync_region: 'us-east-1',
+		'https://g7hio3ov5vfw3hixouz7wbzg6y.appsync-api.eu-central-1.amazonaws.com/graphql',
+	aws_appsync_region: 'eu-central-1',
 	aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
 })
 
